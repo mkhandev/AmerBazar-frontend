90e79e902309546d2e7c2d3d614dd537e5cf56e9
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2025-11-25 01:24:59 +0600

    add admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,29 +1,14 @@
-import AdminUserNavigation from "@/components/navbar/AdminUserNavigation";
-import Logo from "@/components/navbar/Logo";
-import MenuDrawer from "@/components/navbar/MenuDrawer";
-import Navbar from "@/components/navbar/Navbar";
-import UserMenu from "@/components/navbar/UserMenu";
+"use server";
 
-export default function AdminLayout({
+import AdminLayoutWrapper from "@/app/admin/AdminLayoutWrapper";
+import { requireAdmin } from "@/lib/constants/admin-auth-guard";
+
+export default async function AdminLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  return (
-    <div className="flex flex-col">
-      <nav className="w-full border-b-1 py-3 bg-[var(--body-bg)] border-[var(--nav-border)]">
-        <div className="container flex flex-row items-center justify-between w-full gap-1 px-2 mx-auto sm:px-0">
-          <div className="flex-row items-center hidden gap-5 lg:flex">
-            <Logo />
-            <AdminUserNavigation />
-          </div>
+}) {
+  await requireAdmin();
 
-          <MenuDrawer />
-          <UserMenu />
-        </div>
-      </nav>
-
-      <div className="container mx-auto border-b">{children}</div>
-    </div>
-  );
+  return <AdminLayoutWrapper>{children}</AdminLayoutWrapper>;
 }
